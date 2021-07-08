f9ae3906d977f4fdc4273471476d71f64979dadd
PedroJSBezerra <pjsoares46@gmail.com>
2021-07-08 20:29:50 -0300

    update

--- src/com/javaquizapp/ui/MainController.java
+++ src/com/javaquizapp/ui/MainController.java
@@ -12,11 +12,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
@@ -24,46 +21,44 @@ import javafx.stage.Stage;
 //FXML Controller class
 public class MainController implements Initializable {
 
-    @FXML
-    private AnchorPane anchorPane;
-    
     private Stage stage;
     private Scene scene;
     private Parent root;
-    @FXML
-    private Button buttonPrevious;
-    @FXML
-    private Button buttonStart;
-    @FXML
-    private ToggleGroup groupCategory;
-    @FXML
-    private RadioButton rbStrangerThings;
+    
     @FXML
     private TextField textName;
-     
-    @Override   //Initializes the controller class.
+    @FXML
+    private AnchorPane anchorPane;
+    
+    //Initializes the controller class.
+    @Override   
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    
-    @FXML   //button sound
+    /*======================== S O U N D =========================*/
+    @FXML   
     public void strangerThings(ActionEvent event){
         AudioClip stranger = new AudioClip(getClass().getResource("SoundStranger.mp3").toExternalForm());
+        stranger.stop();
         stranger.play();
     }
-        public void logout(ActionEvent event){
+    /*======================== L O G O U T =========================*/
+    @FXML
+    public void logout(ActionEvent event){
         Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Logout");
         alert.setHeaderText("Seus dados não foram salvos!");
         alert.setContentText("Deseja sair agora?");
+        
         if(alert.showAndWait().get() == ButtonType.OK){
             stage = (Stage) anchorPane.getScene().getWindow();
             System.out.println("You successfully logged out!");
             stage.close();
         }
     }
-    @FXML //switch Scenes
-    private void switchToPageStart(ActionEvent event) throws IOException{
+    /*======================== N A V I G A T I O N =========================*/
+    @FXML 
+    public void switchToPageStart(ActionEvent event) throws IOException{
         root = FXMLLoader.load(getClass().getResource("PageStart.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -71,7 +66,7 @@ public class MainController implements Initializable {
         stage.show();
     }
     @FXML
-    private void switchToPageRegister(ActionEvent event) throws IOException {
+    public void switchToPageRegister(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("PageRegister.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -79,7 +74,7 @@ public class MainController implements Initializable {
         stage.show();
     }
     @FXML
-    private void switchToPageGame(ActionEvent event) throws IOException {
+    public void switchToPageGame(ActionEvent event) throws IOException {
         String playerName = textName.getText();
         System.out.println(playerName);
         
@@ -90,7 +85,7 @@ public class MainController implements Initializable {
         stage.show();
     }
     @FXML
-    private void switchToPageRank(ActionEvent event) throws IOException {
+    public void switchToPageRank(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("PageRank.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -98,12 +93,12 @@ public class MainController implements Initializable {
         stage.show();
     }
     @FXML
-    private void switchToPageCredits(ActionEvent event) throws IOException {
+    public void switchToPageCredits(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("PageCredits.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
+    /*=============================== E N D =========================================*/
 }
